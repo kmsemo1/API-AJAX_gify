2eb44fd9f91091eb4d3697dd0b3fb0355fb0688e
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-12-05 20:30:25 -0800

    array in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// create array with my own favorite topics
+var topics = ["Kanye West", "Cam'ron", "Nas", "Jay Z"];
+
+// function to display gify
+function displayGify() {
+    // grabbing text from user input
+    var gify = $(this).attr("")
+}
