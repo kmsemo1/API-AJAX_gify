3e8bbdd6747ebee27d594afd9ba5ccea6607b869
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-12-05 21:37:36 -0800

    index.html fixed

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ var topics = ["Kanye West", "Cam'ron", "Nas", "Jay Z"];
 // function to display gify
 function displayGify() {
     // grabbing text from user input
-    var gify = $(this).attr("")
+    var giphy = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=FiF9XbvehQjHflcIQwufYvHFgt1JMsZT&limit=5";
+
+console.log(topics)
+
 }
