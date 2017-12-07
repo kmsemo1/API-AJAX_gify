2a271492309a131f0a71d79f173589c7294efc7d
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-12-06 21:29:18 -0800

    renderButton function added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+$(document).on("click", function(){
 // create array with my own favorite topics
 var topics = ["Kanye West", "Cam'ron", "Nas", "Jay Z"];
 
@@ -12,7 +13,38 @@ $.ajax({
     url: queryURL,
     method: 'GET'
 }).done(function(response){
-    console.log(response);
+    
+    var giphyDiv = $("<div class='giph'>");
+    
+
 });
 
 }
+
+      // Function for displaying giphy data
+      function renderButtons() {
+        
+                // Deleting the movies prior to adding new movies
+                // (this is necessary otherwise you will have repeat buttons)
+                $("#buttons-view").empty();
+        
+                // Looping through the array of movies
+                for (var i = 0; i < movies.length; i++) {
+        
+                  // Then dynamicaly generating buttons for each movie in the array
+                  // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+                  var a = $("<button>");
+                  // Adding a class of movie to our button
+                  a.addClass("movie");
+                  // Adding a data-attribute
+                  a.attr("data-name", movies[i]);
+                  // Providing the initial button text
+                  a.text(movies[i]);
+                  // Adding the button to the buttons-view div
+                  $("#buttons-view").append(a);
+                }
+              }
+
+})
+
+
