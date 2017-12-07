a00e14a14fc12bc4addf53820c18315ce957f60a
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-12-07 10:02:47 -0800

    fixed console.log response

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-$(document).on("click", function(){
 // create array with my own favorite topics
 var topics = ["Kanye West", "Cam'ron", "Nas", "Jay Z"];
 
@@ -8,43 +7,49 @@ function displayGify() {
     var giphy = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=FiF9XbvehQjHflcIQwufYvHFgt1JMsZT";
 
-// create ajax call
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).done(function(response){
-    
-    var giphyDiv = $("<div class='giph'>");
-    
+    // create ajax call
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
 
-});
+    }).done(function (response) {
+        console.log(response);
+
+        var giphDiv = $("<div class='giphy'>");
+         
+        var gif = response.data
+        for (i = 0; i < topics.length ; i++) {
+            var p = $("<p>").text(gif[i].embed_url)            )
+        }
+
+    });
 
 }
+displayGify();
 
-      // Function for displaying giphy data
-      function renderButtons() {
-        
-                // Deleting the movies prior to adding new movies
-                // (this is necessary otherwise you will have repeat buttons)
-                $("#buttons-view").empty();
-        
-                // Looping through the array of movies
-                for (var i = 0; i < movies.length; i++) {
-        
-                  // Then dynamicaly generating buttons for each movie in the array
-                  // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-                  var a = $("<button>");
-                  // Adding a class of movie to our button
-                  a.addClass("movie");
-                  // Adding a data-attribute
-                  a.attr("data-name", movies[i]);
-                  // Providing the initial button text
-                  a.text(movies[i]);
-                  // Adding the button to the buttons-view div
-                  $("#buttons-view").append(a);
-                }
-              }
+// Function for displaying giphy data
+function renderButtons() {
 
-})
+    // Deleting the movies prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#btn").empty();
+
+    // Looping through the array of movies
+    for (var i = 0; i < topics.length; i++) {
+
+        // Then dynamicaly generating buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class of movie to our button
+        a.addClass("movie");
+        // Adding a data-attribute
+        a.attr("data-name", topics[i]);
+        // Providing the initial button text
+        a.text(topics[i]);
+        // Adding the button to the buttons-view div
+        $("#btn").append(a);
+    }
+}
+
 
 
